7e0beb3132166d918aa350a27583784f7b83ae82
Rgbjz <lukasevoles@gmail.com>
2024-04-25 02:01:53 +0300

    lab6

---
+++ lab6/webpack-project/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.styl$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",          
+          "stylus-loader",
+        ],
+      },      
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "webpack Boilerplate",
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "webpack Boilerplate",
+      template: path.resolve(__dirname, "./src/news.html"),
+      filename: "news.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "webpack Boilerplate",
+      template: path.resolve(__dirname, "./src/photo.html"),
+      filename: "photo.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "webpack Boilerplate",
+      template: path.resolve(__dirname, "./src/rozklad.html"),
+      filename: "rozklad.html",
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ["dist/*"],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets/images"),
+          to: path.resolve(__dirname, "dist/assets/images"),
+        },
+        {
+          from: path.resolve(__dirname, "src/css/style.styl"),
+          to: path.resolve(__dirname, "dist/css"),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
+      chunkFilename: "css/[id].css",
+    }),
+  ],
+};
