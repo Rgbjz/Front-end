3d6345b37808067e96185b95a170a15aee11ab80
Rgbjz <lukasevoles@gmail.com>
2024-04-26 15:40:56 +0300

    lab8

---
+++ lab8/simple-react-app/src/components/Expenses.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+
+const Expenses = (props) => {
+  return (
+    <div className="card expenses">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Expenses;
